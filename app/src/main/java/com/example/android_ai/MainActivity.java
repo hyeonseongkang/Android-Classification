d08758cd7f8582a32fb625ff09bcc6b9d17c27e0
hyeonseongkang <gt9697@naver.com>
2021-12-06 10:08:03 +0900

    test

--- app/src/main/java/com/example/android_ai/MainActivity.java
+++ app/src/main/java/com/example/android_ai/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.android_ai;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "TEST");
+            }
+        });
     }
 }
