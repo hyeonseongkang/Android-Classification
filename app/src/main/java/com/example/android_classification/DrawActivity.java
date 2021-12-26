e3393ed488042d6825abcf0da37bc902261921d3
hyeonseongkang <gt9697@naver.com>
2021-12-26 23:02:17 +0900

    feat: DrawActivity

--- app/src/main/java/com/example/android_classification/DrawActivity.java
+++ app/src/main/java/com/example/android_classification/DrawActivity.java
@@ -2,13 +2,53 @@ package com.example.android_classification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.divyanshu.draw.widget.DrawView;
+
+import java.util.Locale;
 
 public class DrawActivity extends AppCompatActivity {
+
+    private static String TAG = "DrawActivity";
+
+    private DrawView drawView;
+    private Button classifyButton, clearButton;
+    private TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
+
+        drawView = (DrawView) findViewById(R.id.drawView);
+        classifyButton = (Button) findViewById(R.id.classifyButton);
+        clearButton = (Button) findViewById(R.id.clearButton);
+        resultText = (TextView) findViewById(R.id.resultText);
+
+        drawView.setStrokeWidth(100.0f);
+        drawView.setBackgroundColor(Color.BLACK);
+        drawView.setColor(Color.WHITE);
+
+        classifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bitmap image = drawView.getBitmap();
+
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawView.clearCanvas();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android_classification/MainActivity.java
+++ app/src/main/java/com/example/android_classification/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android_classification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,10 +13,21 @@ public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "MainActivity";
 
+    private Button drawButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawButton = (Button) findViewById(R.id.drawButton);
+        drawButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DrawActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
