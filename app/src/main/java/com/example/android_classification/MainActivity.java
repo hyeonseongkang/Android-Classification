902030556b6676ee3816d32172aef5816b4f45b5
hyeonseongkang <gt9697@naver.com>
2021-12-26 17:06:41 +0900

    feat : Image Classification Model ADD

---
+++ app/src/main/java/com/example/android_classification/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.android_classification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "contribution");
+                Toast.makeText(MainActivity.this, "gitTEST", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
